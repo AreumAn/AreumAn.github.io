b1eef3e0623f1426f38f88f3ce8b7b8145a97712
Areum An <areum.an.0928@gmail.com>
2021-07-09 01:07:20 -0400

    Add 404, about and set up version1

---
+++ _pages/about.md
@@ -0,0 +1,19 @@
+---
+title: "About"
+excerpt: "About me and blog"
+
+# toc: true
+# toc_sticky: true
+ 
+date: 2021-07-06
+last_modified_at: 2021-07-06
+
+permalink: /about/
+layout: single
+author_profile: true
+---
+
+---
+I am from South Korea an now working in Toronto.  
+This blog is for recording about my work and life.
+
--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,1 +1,7 @@
-var store = []
+var store = [{
+        "title": "블로그 시작!",
+        "excerpt":"목적  예전부터 찾아 본 걸 또 찾아 보고 또 찾아 보는 일이 많고, 이것저것 해보는데 그게 모두 증발해 가는 느낌이 들었다. 정말 좋은 회사에서 일하기위해서 보여주기식 취업용으로 가지고 있던 포트폴리오 사이트를 삭제하고, 정말 공부했던 것과 느낀 것들을 기록하고 싶어서 블로그를 시작하기로 했다. 지식 공유라고 하기엔 아직 너무나 아는 게 없고..   정말 나를 위해 내가 다시 찾아보기 쉽게, 내가 지금 배우는 것들은 대충 넘기지 않고 조금 더 집중해서 차근차근 살필 수 있도록 만든 블로그다.     컨텐츠 분류  일단 지금 생각하고 있는 컨텐츠들은 이렇다.  뭐… 충분히 나중에 바뀔 가능성이 엄청 많음!      영문 컨텐츠            TECH: IT 관련된 모든 것들 - integration, test, paradigm etc       TUTORIALS: 연습해보기           한글 컨텐츠            ENGINEERING: 기술 블로그 - 캐나다에서 봤던 인터뷰들도 좀 정리해 볼 생각이다.       WORK CULTURE: 회사에서 하는 다양한 workshop, bookclub, D&amp;I       CANADA: CELPIP, 영주권이야기 등등       BLOG: 그냥 이것저것           MEMO            SHORTCUT       CODING       TROUBLESHOOTING          ","categories": ["Blog"],
+        "tags": ["Blog","Toronto","Canada","Programmer"],
+        "url": "/blog/about/",
+        "teaser": null
+      }]
